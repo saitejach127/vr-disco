3192aa6a4271aa474e3f9a531dca6f8a5295f9b8
saitejach127 <saitejach127@gmail.com>
2020-07-27 23:53:48 +0530

    Adding files

---
+++ index.js
@@ -0,0 +1,45 @@
+const $ = (query) => document.querySelector(query);
+
+const sphere = $('a-entity');
+const plane = $('a-plane');
+const sky = $('a-sky')
+const obj = document.getElementById("tree-obj");
+const mtl = document.getElementById("tree-mtl");
+
+const shiftDegrees = (value) => (value + 1) % 360;
+
+let degrees = 0;
+
+// const animate = () => {
+//   degrees = shiftDegrees(degrees);
+//   const color = `hsl(${degrees}, 100%, 50%)`;
+//   const variation = Math.sin(Date.now() / 1000);
+//   const rotation = `-90 0 ${degrees}`;
+
+//   sphere.setAttribute('color', color);
+
+// //   plane.setAttribute('color', color);
+// //   sky.setAttribute('color', color);
+
+//   requestAnimationFrame(animate);
+// };
+
+// requestAnimationFrame(animate);
+
+setInterval(() => {
+    degrees = shiftDegrees(degrees);
+  const color = `hsl(${degrees}, 100%, 50%)`;
+  const color2 = `hsl(${2*degrees}, 100%, 50%)`;
+  const rotaion = `0 ${degrees} 0`
+
+  sphere.setAttribute('color', color);
+  sphere.setAttribute('rotation', rotaion)
+//   sky.setAttribute('color', color);
+},10);
+let i=2;
+setInterval(() => {
+    obj.setAttribute("src", `images/disco_ball_${i}.obj`);
+    mtl.setAttribute("src", `images/disco_ball_${i}.mtl`);
+    i+=1;
+    i = i%3 + 1;
+}, 1000)
