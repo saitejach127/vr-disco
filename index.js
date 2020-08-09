45d88954aa6810398e6f153a1fd40bc08c916f68
saitejach127 <saitejach127@gmail.com>
2020-08-09 16:48:00 +0530

    Lights Changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const shiftDegrees = (value) => (value + 1) % 360;
 
 let degrees = 0;
 let time = 0.0;
-let totalTime = 1;
+let totalTime = 100;
 
 setInterval(() => {
     degrees = shiftDegrees(degrees);
@@ -26,21 +26,39 @@ setInterval(() => {
 
 var i = 1;
 
+// setInterval(() => {
+//   if(i==4){
+//     i = 1;
+//   }
+//   if(i==1){
+//     setAllHidden();
+//     one.setAttribute("visible","true");
+//     i++;
+//   } else if(i==2){
+//     setAllHidden();
+//     two.setAttribute("visible", "true");
+//     i++;
+//   } else if(i==3){
+//     setAllHidden();
+//     three.setAttribute("visible", "true");
+//     i++;
+//   }
+// }, 300)
+
 setInterval(() => {
-  if(i==4){
-    i = 1;
+  if(time >= totalTime){
+    time = 0;
   }
-  if(i==1){
-    setAllHidden();
-    one.setAttribute("visible","true");
-    i++;
-  } else if(i==2){
-    setAllHidden();
-    two.setAttribute("visible", "true");
-    i++;
-  } else if(i==3){
-    setAllHidden();
-    three.setAttribute("visible", "true");
-    i++;
-  }
-}, 300)
+  time += 0.01;
+}, 10);
+
+setInterval(() => {
+  let angle = (360/totalTime) * time;
+  let x = Math.sin(angle);
+  let z = Math.cos(angle);
+  one.setAttribute("position", `${3*x} 3 ${3*z}`);
+  two.setAttribute("position", `${3*z} 3 ${3*x}`)
+  degrees = shiftDegrees(degrees);
+  one.setAttribute("color", `hsl(${degrees}, 100%, 50%)`);
+  two.setAttribute("color", `hsl(100%, 50%, ${degrees})`)
+}, totalTime)
