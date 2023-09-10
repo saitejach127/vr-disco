a170c7a44f5b389a4e99883393329b62ef65bc7f
saitejach127 <saitejach127@gmail.com>
2023-09-10 17:47:50 +0530

    Adjusted camera

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const $ = (query) => document.querySelector(query);
 
-const sphere = $('a-entity');
+const sphere = $("a-entity");
 const one = document.getElementById("1");
 const two = document.getElementById("2");
 const three = document.getElementById("3");
 
-function setAllHidden(){
+function setAllHidden() {
   one.setAttribute("visible", "false");
   two.setAttribute("visible", "false");
   three.setAttribute("visible", "false");
@@ -18,56 +18,56 @@ let time = 0.0;
 let totalTime = 100;
 
 setInterval(() => {
-    degrees = shiftDegrees(degrees);
-  const rotaion = `0 ${degrees} 0`
+  degrees = shiftDegrees(degrees);
+  const rotaion = `0 ${degrees} 0`;
 
-  sphere.setAttribute('rotation', rotaion)
-},10);
+  sphere.setAttribute("rotation", rotaion);
+}, 10);
 
 var i = 1;
 
 setInterval(() => {
-  if(time >= totalTime){
+  if (time >= totalTime) {
     time = 0;
   }
   time += 0.01;
 }, 10);
 
 setInterval(() => {
-  let angle = (360/totalTime) * time;
+  let angle = (360 / totalTime) * time;
   let x = Math.sin(angle);
   let z = Math.cos(angle);
-  one.setAttribute("position", `${3*x} 3 ${3*z}`);
-  two.setAttribute("position", `${3*z} 3 ${3*x}`)
+  one.setAttribute("position", `${3 * x} 3 ${3 * z}`);
+  two.setAttribute("position", `${3 * z} 3 ${3 * x}`);
   degrees = shiftDegrees(degrees);
   one.setAttribute("color", `hsl(${degrees}, 100%, 50%)`);
-  two.setAttribute("color", `hsl(100%, 50%, ${degrees})`)
-}, totalTime)
+  two.setAttribute("color", `hsl(100%, 50%, ${degrees})`);
+}, totalTime);
 
 var dancingModels = [
   {
     modelName: "dancing-knight",
     modelFileLocation: "./models/dancing-knight.glb",
     modelScale: 0.5,
-    animationName: "ezgif.com-video-to-gif"
-  }
-]
+    animationName: "ezgif.com-video-to-gif",
+  },
+];
 
 var DANCE_SET_REPEAT_COUNT = 1;
 var takenPositions = new Set();
 
-function getRandomNumber(MIN_NUMBER = -5, MAX_NUMBER = 5){
+function getRandomNumber(MIN_NUMBER = -5, MAX_NUMBER = 5) {
   let min = Math.ceil(MIN_NUMBER);
   let max = Math.floor(MAX_NUMBER);
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-function getUniqueRandomPosition(){
+function getUniqueRandomPosition() {
   var xPos = getRandomNumber();
   var yPos = getRandomNumber();
   var zPos = getRandomNumber();
   var position = `${xPos} 0 ${zPos}`;
-  if(takenPositions.has(position)){
+  if (takenPositions.has(position)) {
     return getUniqueRandomPosition();
   } else {
     takenPositions.add(position);
@@ -75,19 +75,25 @@ function getUniqueRandomPosition(){
   }
 }
 
-function addModelToScene(modelProperties){
+function addModelToScene(modelProperties) {
   console.log("prop", modelProperties.modelName);
   var entityElement = document.createElement("a-entity");
-  entityElement.setAttribute("id", `${modelProperties.modelName}-id`);
   entityElement.setAttribute("gltf-model", `#${modelProperties.modelName}`);
-  entityElement.setAttribute("scale", `${modelProperties.modelScale} ${modelProperties.modelScale} ${modelProperties.modelScale}`);
-  entityElement.setAttribute("animation-mixer", `clip:${modelProperties.animationName}`);
+  entityElement.setAttribute(
+    "scale",
+    `${modelProperties.modelScale} ${modelProperties.modelScale} ${modelProperties.modelScale}`
+  );
+  entityElement.setAttribute(
+    "animation-mixer",
+    `clip:${modelProperties.animationName}`
+  );
   entityElement.setAttribute("position", getUniqueRandomPosition());
   document.getElementById("scene").appendChild(entityElement);
 }
-
-for(let i=0;i<DANCE_SET_REPEAT_COUNT;i++){
-  for(let j=0;j<dancingModels.length;j++){
-    addModelToScene(dancingModels[j]);
+setTimeout(() => {
+  for (let i = 0; i < DANCE_SET_REPEAT_COUNT; i++) {
+    for (let j = 0; j < dancingModels.length; j++) {
+      addModelToScene(dancingModels[j]);
+    }
   }
-}
+}, 500);
